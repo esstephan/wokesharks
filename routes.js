78cfb01b33f3d5858b1901f5d7e668252ef8bd28
Jeff Bentler <bentljm@gmail.com>
2017-01-27 14:27:14 -0800

    Added date property to both schemas and added code to update date property in routes.js

--- routes.js
+++ routes.js
@@ -27,12 +27,16 @@ module.exports = function(app, express) {
   app.post('/linkClick', function(req, res) {
     //set url to url passed into request body
     var url = req.body.url;
-
+    //create new timestamp
+    var date = Date();
     //check to see if the url exists in the Link schema table
     LinkClick.findOne({url: url})
       .then(function(link) {
         //if it exists, update count
         if(link) {
+          //add timestamp
+          link.date.push(date);
+          //update count
           link.count = link.count++;
           //save updates
           link.save(function(err) {
@@ -43,7 +47,8 @@ module.exports = function(app, express) {
         } else {
           var newLink = new LinkClick({
             url: url,
-            count: 1
+            count: 1,
+            date: [date]
           });
           //save new link
           newLink.save(function(err) {
@@ -74,12 +79,16 @@ module.exports = function(app, express) {
   app.post('/pageView', function(req, res) {
     //set url to url passed into request body
     var title = req.body.title;
-
+    //create new timestamp
+    var date = Date();
     //check to see if the url exists in the Link schema table
     PageView.findOne({title: title})
       .then(function(page) {
         //if it exists, update count
         if(page) {
+          //add timestamp
+          page.date.push(date);
+          //update count
           page.count = page.count++;
           //save updates
           page.save(function(err) {
@@ -90,7 +99,8 @@ module.exports = function(app, express) {
         } else {
           var newPage = new PageView({
             title: title,
-            count: 1
+            count: 1,
+            date: [date]
           });
           //save new link
           newPage.save(function(err) {

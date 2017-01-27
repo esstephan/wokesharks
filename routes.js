60b4155f50c89658cd9ff7dbb9b59854144c1ddc
Jeff Bentler <bentljm@gmail.com>
2017-01-26 17:32:43 -0800

    Fixed routing issue, required routes.js in server.js

--- routes.js
+++ routes.js
@@ -1,42 +1,37 @@
 //import app and schema files
-var app = require('server.js');
-var LinkClick = require('./linkClickModel.js')
-var PageView = require('./pageViewModel.js')
+var LinkClick = require('./Models/linkClickModel.js');
+var PageView = require('./Models/pageViewModel.js');
 
-/* linkClick route */
+module.exports = function(app, express) {
 
-//GET request
-app.get('/linkClick', function(req, res) {
-  //do stuff
+  /* linkClick route */
+  //GET request
+  app.get('/linkClick', function(req, res) {
+    //do stuff
+    res.send("HEY IAN")
+  });
 
-  console.log("REQ BODY", req.body)
-  res.send("HEY IAN")
-})
+  //POST request
+  app.post('/linkClick', function(req, res) {
+    //do stuff here
+    res.send("HEY IAN");
+  });
 
-//POST request
-app.post('/linkClick', function(req, res) {
-  //do stuff here
+  /* pageView route */
+  //GET request
+  app.get('/pageView', function(req, res) {
+    //do stuff
+    res.send("HEY IAN");
+  });
 
-  console.log("REQ BODY", req.body)
-  res.send("HEY IAN");
-})
+  //POST request
+  app.post('/pageView', function(req, res) {
+    //do stuff here
+    res.send("HEY IAN");
+  });
 
-/* pageView route */
+}
 
-//GET request
-app.get('/pageView', function(req, res) {
-  //do stuff
-  console.log("REQ BODY", req.body)
-  res.send("HEY IAN");
-})
-
-//POST request
-app.post('/pageView', function(req, res) {
-  //do stuff here
-  console.log("REQ BODY", req.body)
-  res.send("HEY IAN");
-
-})
 
 
 

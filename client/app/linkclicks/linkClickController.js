75dae4a9d51d2e9fb6a242db1935c84bb46fe44b
esstephan <ericastephan@gmail.com>
2017-01-27 16:00:47 -0800

    added console log to fake link generator

--- client/app/linkclicks/linkClickController.js
+++ client/app/linkclicks/linkClickController.js
@@ -1,9 +1,21 @@
-angular.module("linkClickController", [])
-.controller("linkClick", ["$scope", "$http", function($scope, $http) {
+angular.module("wokeshark.linkClick", [])
+.controller("linkClickController", ["$scope", "$http", function($scope, $http) {
   $scope.getLinks = function(page) {
     $http.get('/linkClick')
     .then(function (data) {
       $scope.count = data.count;
     })
   }
+  $scope.sendLink = function(link){
+    console.log(link);
+    var newLink =
+    {url: link.url}
+    date: Date.now()
+  }
+    http.send('/linkClick', link)
+    .then(function (data) {
+      console.log("link sent")
+    })
+  }
+  $scope.getLinks("hello");
 }])

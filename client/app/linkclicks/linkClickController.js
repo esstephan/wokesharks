94e1a844fa8de6e6f7e580c98af0a702a3e88271
esstephan <ericastephan@gmail.com>
2017-01-30 14:47:58 -0800

    Resolved merge conflict in link click controller

--- client/app/linkclicks/linkClickController.js
+++ client/app/linkclicks/linkClickController.js
@@ -1,21 +1,35 @@
 angular.module("wokeshark.linkClick", [])
 .controller("linkClickController", function($scope, $http) {
-  $scope.getLinks = function(page) {
-    $http.get('/linkClick')
+
+//make request for a single link, this will be /products, /addToCart, or /checkout
+  $scope.getLink = function(url) {
+    $http.get('/linkClick'+ url)
     .then(function (data) {
-      $scope.count = data.count;
+      $scope[url].count = data.count;
+      console.log("count of link clicks retrieved from server for ", url, " is ", $scope[url].count);
+      $scope[url].url = data.url;
+      $scope[url].dates = data.date;
     })
-  }
-  $scope.sendLink = function(link){
-    console.log(link);
-    var newLink =
-    {url: link.url
-    date: Date.now()}
-  
-    $http.send('/linkClick', link)
-    .then(function (data) {
-      console.log("link sent")
-    })
-  }
-})
-  //$scope.getLinks("hello");
+  };
+
+//additional functionality: make request for all links at once
+
+//additional (maybe not needed?) functionality: send new link
+  // $scope.sendLink = function(link){
+  //   console.log(link);
+  //   var newLink =
+  //   {url: link.url}
+  //   date: Date.now()
+  // }
+  //   http.send('/linkClick', link)
+  //   .then(function (data) {
+  //     console.log("link sent")
+  //   });
+  // })
+
+  $scope.getLink(/products);
+  $scope.getLink(/addToCart);
+  $scope.getLink(/checkout);
+
+  });
+

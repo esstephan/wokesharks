3ee097295d4b2a9aabf4228263a176f2e7697a6e
Tony Hyde <tonyhyde6@gmail.com>
2017-01-31 07:03:56 -0800

    added cors support

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var routes = require('./routes.js');
 var path = require('path');
 var app = express();
 var fs = require('fs');
+var cors = require ('cors');
 
 //enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 //set app to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));

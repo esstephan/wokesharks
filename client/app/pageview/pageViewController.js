689f421a0ed9d31a733f34db31dd6128cbe90cb0
igeorge <irgeorge@gmx.com>
2017-01-30 18:58:38 -0600

    Fixed controller.

--- client/app/pageview/pageViewController.js
+++ client/app/pageview/pageViewController.js
@@ -1,17 +1,17 @@
 angular.module("wokeshark.pageView", [])
 .controller("pageViewController", function($scope, $http) {
-	$scope.getPage('/pageView', function(page) {
+	$scope.getPage = function(page) {
 		$http.get('/pageView' + page).then(function (data) {
 			$scope[page].title = data.title;
 			$scope[page].count = data.count;
 			$scope[page].date = data.date;
 		})
-	})
+	}
     $scope.getPage('/');
 	$scope.getPage('/products');
     $scope.getPage('/checkout');
 
-	$scope.sendPage('/pageView', function(page) {
+	$scope.sendPage = $http.post('/pageView', function(page) {
 		var newPage = {
 			title: page.title,
 			date: Date.now()

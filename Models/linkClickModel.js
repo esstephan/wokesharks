6b2906bb6b9f6bea6cc3bb0eb64ec0c9414f22b1
Jeff Bentler <bentljm@gmail.com>
2017-01-30 18:43:37 -0800

    Reverted previous change

--- Models/linkClickModel.js
+++ Models/linkClickModel.js
@@ -1,14 +1,14 @@
-//import mongoose and Schema
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// //import mongoose and Schema
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-/* LinkClick Schema */
-//create new linkClick schema
-var linkClickSchema = mongoose.Schema({
-  url: String,
-  count: Number,
-  date: Array
-});
+// /* LinkClick Schema */
+// //create new linkClick schema
+// var linkClickSchema = mongoose.Schema({
+//   url: String,
+//   count: Number,
+//   date: Array
+// });
 
-//define a mongoose Model
-module.exports = mongoose.model('LinkClick', linkClickSchema);
+// //define a mongoose Model
+// module.exports = mongoose.model('LinkClick', linkClickSchema);
--- server.js
+++ server.js
@@ -6,19 +6,6 @@ var path = require('path');
 var app = express();
 var fs = require('fs');
 
-//connect to mongo database via heroku
-mongoose.connect('mongodb://heroku_6zbmk6ll:qe3h9hugfbbro9rs5i6n861p5r@ds137759.mlab.com:37759/heroku_6zbmk6ll');
-
-//open a mongoose connection
-var db = mongoose.connection;
-
-//handle errors on connection
-db.on('error', console.error.bind(console, 'connection error'));
-//handle successful connection
-db.once('open', function() {
-  console.log('Successfuly connected to database')
-});
-
 //enable CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");

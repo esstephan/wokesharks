463868fe4f13d4fd0bc7eb2ad1469585e3e97e2e
Jeff Bentler <bentljm@gmail.com>
2017-01-30 16:26:15 -0800

    Changed mongodb

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 //connect to mongo database via heroku
-mongoose.connect('mongodb://heroku_dvqdh9sw:12r565q0l5b8dq6sf383d1mtm4@ds137759.mlab.com:37759/heroku_dvqdh9sw');
+mongoose.connect('mongodb://heroku_6zbmk6ll:qe3h9hugfbbro9rs5i6n861p5r@ds137759.mlab.com:37759/heroku_6zbmk6ll');
 //mongoose.connect('mongodb://localhost:8080/data/db');
 
 //open a mongoose connection

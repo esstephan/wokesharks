d559fa1493dd3c8e2012ab4a7ff8390f47a1ef5f
Jeff Bentler <bentljm@gmail.com>
2017-01-31 09:44:37 -0800

    Added cors

--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 //connect to mongo database via heroku
 mongoose.connect('mongodb://heroku_6zbmk6ll:qe3h9hugfbbro9rs5i6n861p5r@ds137759.mlab.com:37759/heroku_6zbmk6ll');
-//mongoose.connect('mongodb://localhost:8080/data/db');
+//mongoose.connect('mongodb://localhost/data/db');
 
 //open a mongoose connection
 var db = mongoose.connection;
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var routes = require('./routes.js');
 var path = require('path');
+var cors = require ('cors');
 var app = express();
 var fs = require('fs');
 
 //enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 //set app to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));

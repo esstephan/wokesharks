82b8f91f41073ef6e1f4dad0bc7e1ad36725fd95
Jeff Bentler <bentljm@gmail.com>
2017-02-01 12:12:04 -0800

    Added Links and Paths factory

---
+++ client/app/factory.js
@@ -0,0 +1,67 @@
+angular.module('wokeshark.factory', [])
+
+/* LINKS FACTORY */
+.factory('Links', function ($http) {
+
+  //should get all links from database
+  var getAllLinks = function () {
+    return $http({
+      method: 'GET',
+      url: '/linkClickAll'
+    })
+    .then(function (response) {
+      return response.data;
+    });
+  };
+
+  //should get a specified link from database
+  var getLink = function (url) {
+    return $http({
+      method: 'GET',
+      url: '/linkClick',
+      params: {url: url}
+    })
+    .then(function (response) {
+      return response;
+    });
+  };
+
+  return {
+    getAllLinks: getAllLinks,
+    getLink: getLink
+  };
+})
+
+
+
+/* PAGES FACTORY */
+.factory('Pages', function ($http) {
+
+  //should get all links from database
+  var getAllPages = function () {
+    return $http({
+      method: 'GET',
+      url: '/pageViewAll'
+    })
+    .then(function (response) {
+      return response.data;
+    });
+  };
+
+  //should get a specified link from database
+  var getPage = function (title) {
+    return $http({
+      method: 'GET',
+      url: '/pageView',
+      params: {title: title}
+    })
+    .then(function (response) {
+      return response;
+    });
+  };
+
+  return {
+    getAllPages: getAllPages,
+    getPage: getPage
+  };
+})

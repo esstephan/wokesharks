811a67fba7bd0df299e950a1cb435b22bb4befd9
igeorge <irgeorge@gmx.com>
2017-02-02 18:43:09 -0600

    Added PageViewVisual.

--- client/app/pageview/pageViewVisualsController.js
+++ client/app/pageview/pageViewVisualsController.js
@@ -1,9 +1,46 @@
 angular.module('wokeshark.pageViewPlotly', [])
 
-.controller('pageViewVisualsController', function($scope) {
-	$scope.data = [{
-		x: [1, 2, 3, 4],
-		y: [10, 14, 22, 50]
-	}]
+.controller('pageViewVisualsController', function($scope, Pages) {
+	var allTitles = [];
+	var allCounts = [];
+
+	$scope.refresh = function() {
+		Pages.getAllPages().then(function (res) {
+			res.data.forEach(function (element) {
+				allTitles.push(element.title);
+				allCounts.push(element.count)
+			})
+			$scope.data = [{
+				name: "Page View", 
+				x: allTitles, 
+				y: allCounts,
+				type: 'bar'
+			}];
+		})
+	};
+	$scope.refresh();
+})
+
+.directive('linePlot', function() {
+	function pageFunc(scope, element, attributes) {
+		scope.$watch('data', function (plots) {
+			var layout = {
+				title: 'Page View',
+				yaxis: {
+					title: 'Count',
+					titlefront: 'Arial'
+				},
+				width: 600,
+				height: 300, 
+				margin: {'t': 40, 'b': 20, 'l': 40, 'r': 0}
+			};
+
+			Plotly.newPlot(element[0], plots, layout)
+		}, true)
+	}
+
+	return {
+		page: pageFunc
+	};
 })
 

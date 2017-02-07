29f7bfcb74209f2edb51feb32fd0692967aa6aa5
Ian George <OppenheimerAndTheMartians@users.noreply.github.com>
2017-02-06 19:01:21 -0600

    Update pageViewVisualsController.js

--- client/app/pageview/pageViewVisualsController.js
+++ client/app/pageview/pageViewVisualsController.js
@@ -1,17 +1,18 @@
 angular.module('wokeshark.pageViewPlotly', [])
 
-.controller('pageViewVisualsController', function($scope, Pages) {
-	var allTitles = [];
+.controller('pageViewVisualsController', function($scope, Pages) { // our bar graph controller
+
+	var allTitles = []; // initialization block
 	var allCounts = [];
 
-	$scope.refresh = function() {
-		Pages.getAllPages().then(function (res) {
-			res.data.forEach(function (element) {
-				allTitles.push(element.title);
+	$scope.refresh = function() { // When refresh is called:
+		Pages.getAllPages().then(function (res) { // Get all the pages
+			res.data.forEach(function (element) { // For all items in response.data...
+				allTitles.push(element.title); // Push the page and the count properties onto their relevant arrays.
 				allCounts.push(element.count)
 			})
-			$scope.data = [{
-				x: allTitles,
+			$scope.data = [{ // Set our bar graph parameters within an object...
+				x: allTitles, // and store it-as Plotly requires-within an array.
 				y: allCounts,
 				type: 'bar'
 			}];
@@ -20,23 +21,23 @@ angular.module('wokeshark.pageViewPlotly', [])
 	$scope.refresh();
 })
 
-.controller("pageViewPieController", function ($scope, Pages) {
+.controller("pageViewPieController", function ($scope, Pages) { // our pie graph controller
 
-  var allTitles = [];
+  var allTitles = []; // initialization block
   var allCounts = [];
   var percentage = [];
   var totalCount = 0;
 
-  $scope.refresh = function() {
-    Pages.getAllPages()
+  $scope.refresh = function() { // When refresh is called:
+    Pages.getAllPages() // Get all the pages
       .then(function(response) {
-        response.data.forEach(function(item) {
-          totalCount+=item.count;
+        response.data.forEach(function(item) { // For all the items in response.data...
+          totalCount+=item.count; // Increment the total amount of clicks by the amount of times the current page has been clicked
           allTitles.push(item.title);
           allCounts.push(item.count);
         })
-        allCounts.forEach(function(count) {
-          percentage.push(count/totalCount*100)
+        allCounts.forEach(function(count) { // For each page...
+          percentage.push(count/totalCount*100); // Get the percentage of how many times this page has been clicked over the total number of aggregate clicks.
         })
         $scope.data = {values: percentage, labels: allTitles, type: 'pie'};
         $scope.data = [$scope.data];
@@ -49,20 +50,20 @@ angular.module('wokeshark.pageViewPlotly', [])
 
 .controller('pageViewDayController', function($scope, Pages) {
 
-  var allTitles = [];
+  var allTitles = []; // initialization block
   var allDates = [];
   var everyDay = [];
   var dates = {};
 
   $scope.refresh = function() {
-    Pages.getAllPages().then(function(res) {
-      res.data.forEach(function (element) {
-        element.date.forEach(function (specificElement) {
-          allDates.push(specificElement);
+    Pages.getAllPages().then(function(res) { // get all pages...
+      res.data.forEach(function (element) { // for each element in the data object...
+        element.date.forEach(function (specificElement) { // for each item in the element...
+          allDates.push(specificElement); // push onto the array
         })
     })
     allDates.forEach(function (day) {
-      var day = day.slice(4, 10);
+      var day = day.slice(4, 10); // Get the relevant substring: for example, "Feb 06"
       if(dates[day]) {
         dates[day] = dates[day] + 1;
       } else {
@@ -84,5 +85,44 @@ angular.module('wokeshark.pageViewPlotly', [])
 })
 
 
+.directive('linePlot', function () {
+  // Create a page function
+  function pageFunc(scope, element, attrs) {
+      scope.$watch('data', function (plots) {
+        var layout = {
+          width: 600,
+          height: 300,
+          margin: { 't': 40, 'b':20, 'l':40, 'r':0 },
+        };
 
+      Plotly.newPlot(element[0], plots, layout);
+    }, true);
+  }
 
+  // Return this function for linking ...
+  return {
+      page: pageFunc
+  };
+
+})
+
+.directive('overallPlot', function () {
+  // Create a page function
+  function pageFunc(scope, element, attrs) {
+      scope.$watch('data', function (plots) {
+        var layout = {
+          width: 400,
+          height: 200,
+          margin: { 't': 40, 'b':20, 'l':40, 'r':0 },
+        };
+
+      Plotly.newPlot(element[0], plots, layout);
+    }, true);
+  }
+
+  // Return this function for linking ...
+  return {
+      page: pageFunc
+  };
+
+})
